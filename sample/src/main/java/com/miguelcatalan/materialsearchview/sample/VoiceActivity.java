777da335ab8e0320d88ba85ef7e22fd39ac06883
Kevin Clark <kevinclark311@gmail.com>
2019-06-27 11:17:01 -0400

    AndroidX migration

--- sample/src/main/java/com/miguelcatalan/materialsearchview/sample/VoiceActivity.java
+++ sample/src/main/java/com/miguelcatalan/materialsearchview/sample/VoiceActivity.java
@@ -2,10 +2,10 @@ package com.miguelcatalan.materialsearchview.sample;
 
 import android.content.Intent;
 import android.speech.RecognizerIntent;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
